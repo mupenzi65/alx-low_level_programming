51d2ff183606ea4fef9b4adb9b811b2cf5b137a1
mupenzi65 <59319372+mupenzi65@users.noreply.github.com>
2022-03-25 18:15:45 +0200

    changed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,4 +26,17 @@ char *cap_string(char *s)
  * check_seperators - Separators of words: space, tabulation, new line,
  * ,, ;, ., !, ?, ", (, ), {, and }
  * @c: an input character
- * 
+ * Return: 1 if seperator, 0 otherwise
+ */
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
+} 
